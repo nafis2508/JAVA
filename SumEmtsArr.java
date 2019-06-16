67a4685a20586466ca0d0d9992ad91555562ae95
Muntasir Md. Nafis <46222806+nafis2508@users.noreply.github.com>
2019-06-16 23:57:53 +0600

    Add files via upload

---
+++ SumEmtsArr.java
@@ -0,0 +1,13 @@
+package homepractice;
+import java.util.*;
+public class SumEmtsArr {
+ public static void main (String[ ] args)
+ {
+	 int a [ ] = { 2 , 45 , 67 } , sum = 0 ;
+	 for (int i = 0 ; i < a.length ; i++)
+	 {
+		 sum + = a[i] ;
+	 }
+	 System.out.print(sum);
+ }
+}
