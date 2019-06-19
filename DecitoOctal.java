66a501964b503da611774805455a179815a7c292
Muntasir Md. Nafis <46222806+nafis2508@users.noreply.github.com>
2019-06-20 03:19:19 +0600

    Add files via upload

---
+++ DecitoOctal.java
@@ -0,0 +1,25 @@
+package W3resourcesSoln;
+	import java.util.*;
+	import java.io.*;
+	import java.lang.*;
+	class DecitoOctal
+	{
+		public static void main (String [] args)
+		{
+			Scanner input = new Scanner (System.in) ;
+			int number = input.nextInt( ) ;
+			int octal_num[ ] = new int [100] , i = 0 ;
+			while (number != 0)
+			{
+				octal_num [i] = number % 8 ;
+				number = number / 8 ;
+				i++ ;
+			}
+			System.out.print("the octal number is : ") ;
+			for(int j = i-1 ;j>=0;j--)
+			{
+			System.out.print ( octal_num[j]);
+		     }
+		}
+	}
+
