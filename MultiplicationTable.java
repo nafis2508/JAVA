3564b8fcb6b03987fca4acdf781acea84b64911c
Muntasir Md. Nafis <46222806+nafis2508@users.noreply.github.com>
2019-06-20 01:37:47 +0600

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,17 @@
+package W3resourcesSoln;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class MultiplicationTable
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input = new Scanner (System.in);
+		int a = input.nextInt() , result=0;
+	     for (int i = 0 ;i<11 ;i++)
+	     {
+	       System.out.println(a + "*" + i + "=" + a*i);
+	     }
+	    
+}
+}
