684fe4fb6fc50dddd251672303b2304d7c0df7ed
Muntasir Md. Nafis <46222806+nafis2508@users.noreply.github.com>
2019-06-20 23:19:37 +0600

    Add files via upload

---
+++ PosOrNeg.java
@@ -0,0 +1,15 @@
+package W3resourcesSoln;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class PosOrNeg {
+   public static void main (String[] args)
+   {
+	   Scanner in = new Scanner (System.in) ;
+	   int a = in.nextInt( ) ;
+	   if(a>0) 
+		   System.out.print("positive") ;
+	   else 
+		    System.out.print("negative");
+   }
+}
