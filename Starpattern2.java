4c08a2f106488d1c0851bd40a57086b6c181ba73
Muntasir Md. Nafis <46222806+nafis2508@users.noreply.github.com>
2019-06-21 20:58:19 +0600

    Add files via upload

---
+++ Starpattern2.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+ class Starpattern2 {
+
+	public static void main(String[] args) {
+		 int i , j ;
+          for (i = 1 ;i<=4;i++)
+          {
+        	  for(j=4;j>=i;j--)
+        	  {
+        		  System.out.print("*");
+        	  }
+        	  System.out.println(" ");
+          }
+	}
+
+}
